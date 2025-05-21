4370cc0d931e6c91cf4ce67a1878b998fc28993f
rabbani <rabbani@gmail.com>
2025-05-21 14:30:48 +0530

    helloworld script

---
+++ 2.variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rabbani: Hi Raina I'm Rabbani. Hope you are doing good"
+echo "Raina: Hello nice to meet you. Myself Raina"
+echo "Rabbani: am huge fan of you"
+echo "Raina: thanks, its my pleasure Rabbani"
