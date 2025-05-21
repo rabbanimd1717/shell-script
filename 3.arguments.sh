0c5619c8a2b9d5e097a5862af095c1996c94c2f7
rabbani <rabbani@gmail.com>
2025-05-21 15:34:22 +0530

    helloworld script

---
+++ 3.arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: Hi $person2 I'm $person1. Hope you are doing good"
+echo "$person2: Hello nice to meet you. Myself $person2"
+echo "$person1: am huge fan of you $person2"
+echo "$person1: thanks, its my pleasure $person1"
