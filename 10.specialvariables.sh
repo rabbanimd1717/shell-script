7128397b8e09468e9a245f0dc0bdedd583a9f2d3
rabbani <rabbani@gmail.com>
2025-05-21 19:50:44 +0530

    helloworld script

--- 10.specialvariables.sh
+++ 10.specialvariables.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "all variables: $@" #to ptint all variables
+echo "Number of variables passed: $#"
+echo "to show script name: $0"
+echo "current working directory: $PWD"
+echo "home directory of the current user: $HOME"
+echo "which user is running this script: $USER" 
+echo "hostname: $HOSTNAME"
+echo "process id of the current shell script: $$"
