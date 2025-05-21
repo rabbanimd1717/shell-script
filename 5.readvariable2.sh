e33b79deff5c606adc2ece4dee217949e2a85c45
rabbani <rabbani@gmail.com>
2025-05-21 16:02:39 +0530

    helloworld script

---
+++ 5.readvariable2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username: "
+
+read username
+
+echo "please enter your password: "
+
+read password
+
+echo "username is: $username and password is: $password"
