34396601b4367a1e1d7d00319e113aa63f5adc14
rabbani <rabbani@gmail.com>
2025-05-22 16:15:49 +0530

    helloworld script

--- 12.installation.sh
+++ 12.installation.sh
@@ -5,7 +5,7 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "proceed to run and this is sudo user"
-    exit 1
+    exit 0
 else 
     echo "this is not superuser. just stop this command"
 fi
