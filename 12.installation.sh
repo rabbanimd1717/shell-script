f68fb88c3363d7101a96a278407306fadc65ed98
rabbani <rabbani@gmail.com>
2025-05-22 15:55:02 +0530

    helloworld script

--- 12.installation.sh
+++ 12.installation.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+userid=$(id -u)
+
+if [$userid -eq 0]
+then
+    echo "proceed to run and this is sudo user"
+else 
+    echo "this is not superuser. just stop this command"
 dnf install mysql -y
