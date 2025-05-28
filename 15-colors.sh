4d8993e16752c0f96310625fe8cd8cf7adbc5c28
rabbani <rabbani@gmail.com>
2025-05-28 11:22:39 +0530

    helloworld script

--- 15-colors.sh
+++ 15-colors.sh
@@ -14,9 +14,9 @@ N="\e[0m"
 FUN_NAME(){
     if [ $1 -eq 0 ]
     then
-        echo "$2 is $red SUCCESS $N"
+        echo "$2 is -e $red SUCCESS $N"
     else
-        echo "$2 is $yellow Failure $N"
+        echo "$2 is -e $yellow Failure $N"
         exit 1
     fi
 }
