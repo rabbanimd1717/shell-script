0ac0582786fdf9fa819c9f16d3ebb482ff0352f9
rabbani <rabbani@gmail.com>
2025-05-28 11:10:55 +0530

    helloworld script

--- 15-colors.sh
+++ 15-colors.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/SCRIPT_NAME-TIMESTAMP.log
+
 
 FUN_NAME(){
     if [ $1 -eq 0 ]
@@ -8,23 +12,24 @@ FUN_NAME(){
         echo "$2 is SUCCESS"
     else
         echo "$2 is Failure"
+        exit 1
     fi
 }
 
 echo $USER_ID
 
-dnf install mysql -y 
+dnf install mysql -y &>> $LOG_FILE
 
 FUN_NAME $? "Installing package of mysql"
 
-dnf install git -y
+dnf install git -y &>> $LOG_FILE
 
 FUN_NAME $? "Installing package of git"
 
 echo "Both packages install successfull"
 
-dnf install dockerr -y
+dnf install dockerr -y &>> LOG_FILE
 
-FUN_NAME $? "Installing Docker"
+FUN_NAME $? "Installing Docker" 
 
 echo "I think its failed y we give wrong package"
