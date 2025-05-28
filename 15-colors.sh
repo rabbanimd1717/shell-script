bd157199bc55fddaee7f6aa7eb76b1df8d32af20
rabbani <rabbani@gmail.com>
2025-05-28 11:01:28 +0530

    helloworld script

--- 15-colors.sh
+++ 15-colors.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
+
+FUN_NAME(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 is SUCCESS"
+    else
+        echo "$2 is Failure"
+        exit
+    fi
+}
+
+echo $USER_ID
+
+dnf install mysql -y 
+
+FUN_NAME $? "Installing package of mysql"
+
+dnf install git -y
+
+FUN_NAME $? "Installing package of git"
+
+echo "Both packages install successfull"
+
+dnf install dockerr -y
+
+FUN_NAME $? "Installing Docker"
+
+echo "I think its failed y we give wrong package"
