d4da25e9dcb8dee6d0f9980edec0a0e1fedd503d
rabbani <rabbani@gmail.com>
2025-05-28 11:12:42 +0530

    helloworld script

--- 15-colors.sh
+++ 15-colors.sh
@@ -3,7 +3,7 @@
 USER_ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=/tmp/SCRIPT_NAME-TIMESTAMP.log
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
 FUN_NAME(){
